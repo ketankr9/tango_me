5eca34be12de4cd9bd60db22f74dc40fe384c4fd
ketankr9 <utsav.krishnan.cse15@iitbhu.ac.in>
2016-09-08 20:36:05 +0530

    beforeCreateSuperUser

--- tango_me/settings.py
+++ tango_me/settings.py
@@ -117,6 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATICFILES_DIR=[STATIC_DIR,]
+STATICFILES_DIRS=[STATIC_DIR,]
 
 STATIC_URL = '/static/'
