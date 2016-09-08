4d3767efb0a3b5a8a796d9f20868c37c83e21b63
ketankr9 <utsav.krishnan.cse15@iitbhu.ac.in>
2016-09-08 14:07:04 +0530

    8sep1407

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from rango import views
 
 urlpatterns=[
 url(r'^$',views.rangoo, name='rangoo'),
 url(r'^about/$',views.about,name="abt"),
+#url(r'^static/',)
 
 ]
